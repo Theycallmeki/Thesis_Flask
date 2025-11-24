e15b10ab834c9e6debbf7559dff9d2ab85b7a1ad
donotlook <repodsalgo200zzz@gmail.com>
2025-11-24 17:57:05 +0800

    add: paymongo API (gcash) + dummy item

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
-from routes import items_bp, sales_bp, user_routes
+from routes import items_bp, sales_bp, user_routes, payment_bp
 
 def register_routes(app):
     app.register_blueprint(items_bp, url_prefix='/items')
     app.register_blueprint(sales_bp, url_prefix='/sales')
     app.register_blueprint(user_routes, url_prefix='/users')  
+    app.register_blueprint(payment_bp, url_prefix='/payment')
