10893afb41b12377ab9dca7770c5775f4ca7facc
donotlook <repodsalgo200zzz@gmail.com>
2026-01-03 18:42:17 +0800

    add: DB model for Cash Payment status

---
+++ models/pending_cash_payment.py
@@ -0,0 +1,21 @@
+from db import db
+from datetime import datetime
+from sqlalchemy.dialects.postgresql import JSON
+
+class PendingCashPayment(db.Model):
+    __tablename__ = "pending_cash_payments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+
+    code = db.Column(db.String(6), unique=True, nullable=False)
+    cart = db.Column(JSON, nullable=False)
+
+    status = db.Column(
+        db.Enum("PENDING", "CANCELLED", "PAID", name="cash_status"),
+        default="PENDING",
+        nullable=False
+    )
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    expires_at = db.Column(db.DateTime, nullable=True)
